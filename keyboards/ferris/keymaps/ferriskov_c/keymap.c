8b30af2a8584d65f2228cc06d476c4d8224cd19c
Voskov <Ariel.Voskov@Axonius.com>
2022-06-07 10:28:12 +0300

    add printscreen

--- keyboards/ferris/keymaps/ferriskov_c/keymap.c
+++ keyboards/ferris/keymaps/ferriskov_c/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CONFIG] = LAYOUT( /* [> CONFIG <] */
       KC_TRNS,  KC_ACL0,  KC_ACL1,  KC_ACL2,  KC_TRNS,        KC_WH_U,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_TRNS,
       KC_TRNS,  KC_BTN2,  KC_NO,    KC_BTN1,  KC_TRNS,        KC_WH_D,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_TRNS,
-      RESET,    KC_TRNS,  KC_TRNS,  DF(1),    DF(0),          KC_TRNS,  KC_WH_L,  KC_TRNS,  KC_WH_R,  RESET,
+      RESET,    KC_TRNS,  KC_TRNS,  DF(1),    DF(0),          KC_RPRN,  KC_WH_L,  KC_TRNS,  KC_WH_R,  RESET,
                                     KC_TRNS,  KC_TRNS,        KC_TRNS,  KC_TRNS
   )
 };
